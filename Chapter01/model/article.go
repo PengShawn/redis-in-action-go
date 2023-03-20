0ad2e0a2c9d2c6d0be80d491ac91824ca085533e
pshawn <pshawn@126.com>
2023-03-20 19:33:26 +0800

    增加中文注释

--- Chapter01/model/article.go
+++ Chapter01/model/article.go
@@ -26,6 +26,7 @@ func NewArticleRepo(conn *redis.Client) *ArticleRepo {
 	return &ArticleRepo{Conn: conn}
 }
 
+// ArticleVote 给文章投票
 func (r *ArticleRepo) ArticleVote(article, user string) {
 	// 文章发布一周之后，不再允许投票
 	cutoff := time.Now().Unix() - common.OneWeekInSeconds
@@ -35,17 +36,18 @@ func (r *ArticleRepo) ArticleVote(article, user string) {
 
 	articleId := strings.Split(article, ":")[1]
 	if r.Conn.SAdd("voted:"+articleId, user).Val() != 0 {
-		r.Conn.ZIncrBy("score:", common.VoteScore, article)
-		r.Conn.HIncrBy(article, "votes", 1)
+		r.Conn.ZIncrBy("score:", common.VoteScore, article) // 文章分数增加
+		r.Conn.HIncrBy(article, "votes", 1)                 // 文章投票数增加
 	}
 }
 
+// PostArticle 发布文章
 func (r *ArticleRepo) PostArticle(user, title, link string) string {
 	articleId := strconv.Itoa(int(r.Conn.Incr("article:").Val()))
 
 	voted := "voted:" + articleId
-	r.Conn.SAdd(voted, user)
-	r.Conn.Expire(voted, common.OneWeekInSeconds*time.Second)
+	r.Conn.SAdd(voted, user)                                  // 把发布者添加到已投票用户列表
+	r.Conn.Expire(voted, common.OneWeekInSeconds*time.Second) // 设置过期时间为一周
 
 	now := time.Now().Unix()
 	article := "article:" + articleId
@@ -57,12 +59,14 @@ func (r *ArticleRepo) PostArticle(user, title, link string) string {
 		"votes":  1,
 	})
 
-	r.Conn.ZAdd("score:", &redis.Z{Score: float64(now + common.VoteScore), Member: article})
-	r.Conn.ZAdd("time:", &redis.Z{Score: float64(now), Member: article})
+	r.Conn.ZAdd("score:", &redis.Z{Score: float64(now + common.VoteScore), Member: article}) // 初始化文章分数为时间+投票分数
+	r.Conn.ZAdd("time:", &redis.Z{Score: float64(now), Member: article})                     // 初始化文章发布时间
 	return articleId
 }
 
+// GetArticles 获取文章列表
 func (r *ArticleRepo) GetArticles(page int64, order string) []map[string]string {
+	// 默认按照分数排序
 	if order == "" {
 		order = "score:"
 	}
@@ -70,7 +74,7 @@ func (r *ArticleRepo) GetArticles(page int64, order string) []map[string]string 
 	end := start + common.ArticlesPerPage - 1
 
 	ids := r.Conn.ZRevRange(order, start, end).Val()
-	articles := []map[string]string{}
+	var articles []map[string]string
 	for _, id := range ids {
 		articleData := r.Conn.HGetAll(id).Val()
 		articleData["id"] = id
@@ -79,6 +83,7 @@ func (r *ArticleRepo) GetArticles(page int64, order string) []map[string]string 
 	return articles
 }
 
+// AddRemoveGroups 添加或删除文章所属的组
 func (r *ArticleRepo) AddRemoveGroups(articleId string, toAdd, toRemove []string) {
 	article := "article:" + articleId
 	for _, group := range toAdd {
@@ -89,12 +94,14 @@ func (r *ArticleRepo) AddRemoveGroups(articleId string, toAdd, toRemove []string
 	}
 }
 
+// GetGroupArticles 获取组内文章列表
 func (r *ArticleRepo) GetGroupArticles(group, order string, page int64) []map[string]string {
 	if order == "" {
 		order = "score:"
 	}
 	key := order + group
 	if r.Conn.Exists(key).Val() == 0 {
+		// ZInterStore 将多个有序集合的交集存储在新的有序集合中
 		res := r.Conn.ZInterStore(key, &redis.ZStore{Aggregate: "MAX", Keys: []string{"group:" + group, order}}).Val()
 		if res <= 0 {
 			log.Println("ZInterStore return 0")
@@ -104,6 +111,7 @@ func (r *ArticleRepo) GetGroupArticles(group, order string, page int64) []map[st
 	return r.GetArticles(page, key)
 }
 
+// Reset 重置数据库
 func (r *ArticleRepo) Reset() {
 	r.Conn.FlushDB()
 }
